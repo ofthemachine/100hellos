d987b5dd608a4976fb2d5e4ac82f893b9bfbf378
Tim Frison <tim.frison@clio.com>
2026-02-26 21:05:07 -0700

    Dart says Hello! + fraglet (stdin/args)

--- dart/fraglet/verify_args.sh
+++ dart/fraglet/verify_args.sh
@@ -9,5 +9,6 @@ void main(List<String> args) {
   print("Args: ${args.join(" ")}");
 }
 EOF
-fragletc --image "$IMAGE" "$tmp" foo bar baz 2>&1 | grep -q "Args: foo bar baz"
+output=$(fragletc --image "$IMAGE" "$tmp" foo bar baz 2>&1)
+echo "$output" | grep -q "Args: foo bar baz"
 echo "✓ args verified"
--- dart/fraglet/verify_stdin.sh
+++ dart/fraglet/verify_stdin.sh
@@ -14,5 +14,6 @@ void main() {
   }
 }
 EOF
-echo "hello" | fragletc --image "$IMAGE" "$tmp" 2>&1 | grep -q "HELLO"
+output=$(echo "hello" | fragletc --image "$IMAGE" "$tmp" 2>&1)
+echo "$output" | grep -q "HELLO"
 echo "✓ stdin verified"
